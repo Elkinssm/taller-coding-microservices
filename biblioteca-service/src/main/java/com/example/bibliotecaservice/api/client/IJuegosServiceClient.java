64f6da4b41879b08005ee89394e1374967597a16
Elkin Silva <elkinsilvamanrique@gmail.com>
2023-03-08 22:53:50 -0500

    Add feign client

---
+++ biblioteca-service/src/main/java/com/example/bibliotecaservice/api/client/IJuegosServiceClient.java
@@ -0,0 +1,16 @@
+package com.example.bibliotecaservice.api.client;
+
+import com.example.bibliotecaservice.domain.model.Juegos;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "juego-service")
+public interface IJuegosServiceClient {
+
+    @GetMapping("/juegos/{biblioteca}")
+    ResponseEntity<List<Juegos>> getJuegosByBiblioteca(@PathVariable String biblioteca);
+}
--- biblioteca-service/src/main/java/com/example/bibliotecaservice/api/controller/BibliotecaController.java
+++ biblioteca-service/src/main/java/com/example/bibliotecaservice/api/controller/BibliotecaController.java
@@ -1,8 +1,9 @@
 package com.example.bibliotecaservice.api.controller;
 
-import com.example.bibliotecaservice.api.service.BibliotecaService;
+import com.example.bibliotecaservice.api.client.IJuegosServiceClient;
 import com.example.bibliotecaservice.domain.model.Juegos;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,14 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RequiredArgsConstructor
+@RequestMapping("/biblioteca")
 @RestController
 public class BibliotecaController {
 
-    private final BibliotecaService serviceClient;
+
+    private final IJuegosServiceClient iJuegosServiceClient;
 
     @GetMapping("/{biblioteca}")
     ResponseEntity<List<Juegos>> getBiblioteca(@PathVariable String biblioteca) {
-        return ResponseEntity.ok().body(serviceClient.getJuegoByBiblioteca(biblioteca));
+        return iJuegosServiceClient.getJuegosByBiblioteca(biblioteca);
     }
+
+//    private final BibliotecaService serviceClient;
+//
+//    @GetMapping("/{biblioteca}")
+//    ResponseEntity<List<Juegos>> getBiblioteca(@PathVariable String biblioteca) {
+//        return ResponseEntity.ok().body(serviceClient.getJuegoByBiblioteca(biblioteca));
+//    }
+
 
 }
